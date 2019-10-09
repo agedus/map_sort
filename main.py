b699107e832668e4723f2584bd9506abad612817
agedus <bassemens@gmail.com>
2019-10-09 14:46:57 +0200

    1.0.2

--- main.py
+++ main.py
@@ -3,9 +3,15 @@ import shutil
 
 
 mylist = os.listdir("../../../testrange")
-
 path = "../../../testrange"
+fotos = ["fotos", ".jpeg", ".jpg", ".png"]
+music = ["music", ".mp3"]
+ingepakt = ["ingepakt", ".zip", ".rar"]
+text = ["text", ".txt"]
+file_types = [fotos, music, ingepakt, text]
 
-for x in mylist:
-    if x == "test_txt.txt":
-        shutil.move(f"{path}/{x}", f"{path}/txt")
+for x in file_types:
+    for file_extension in x:
+        for q in mylist:
+            if q.lower().endswith(file_extension):
+                shutil.move(f"{path}/{q}", f"{path}/{x[0]}")
