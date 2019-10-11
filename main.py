ed97d1c7d94c5df2c10ccad8c62c1ac3ad704478
agedus <bassemens@gmail.com>
2019-10-11 16:28:32 +0200

    1.0.3

--- main.py
+++ main.py
@@ -1,17 +1,48 @@
 import os
+import time
 import shutil
 
 
-mylist = os.listdir("../../../testrange")
-path = "../../../testrange"
-fotos = ["fotos", ".jpeg", ".jpg", ".png"]
-music = ["music", ".mp3"]
-ingepakt = ["ingepakt", ".zip", ".rar"]
-text = ["text", ".txt"]
-file_types = [fotos, music, ingepakt, text]
+path = "C:/Users/basse/Downloads"
+fotos = ["jpeg", "jpg", "png"]
+text = ["txt", "pdf"]
+extensions = [{"C:/Users/basse/Pictures/downloads": fotos},
+              {"C:/Users/basse/Documents/downloads": text}]
+skip = ["ini", "part"]
+downloads = os.listdir("C:/Users/basse/Downloads")
 
-for x in file_types:
-    for file_extension in x:
-        for q in mylist:
-            if q.lower().endswith(file_extension):
-                shutil.move(f"{path}/{q}", f"{path}/{x[0]}")
+
+def sorting():
+    for d_file in downloads:
+        file_extension = d_file.split(".")[-1]
+        for i in extensions:
+            for j in i:
+                print(i[j])
+                if file_extension in skip:
+                    print(f" bestand {d_file} skipt")
+                    continue
+                # elif d_file + ".part" in downloads:
+                #     continue
+                # elif file_extension in i[j]:
+                #     print(f"bestand {d_file} gaat naar {j}/{d_file}")
+                #     # shutil.move(f"{path}/{d_file}", f"{j}/{d_file}")
+                # else:
+                #     print(f"bestand {d_file} gaat naar onbekend")
+                #     pass
+                #     # shutil.move(f"{path}/{d_file}",
+                #     #             f"D:/unknown _downloads/{d_file}")
+
+        # while True:
+        #     mylist = os.listdir("C:/Users/basse/Downloads")
+        #     time.sleep(1)
+        #     if "desktop.ini" in mylist:
+        #         if len(mylist) >= 2:
+        #             print(mylist)
+        #             sorting()
+        #     else:
+        #         if len(mylist) >= 1:
+        #             print(mylist)
+        #             sorting()
+
+
+sorting()
